5ffcbcff37cd292b5c60a3eb28979d76ae4d34f9
Note <seeyou85@gmail.com>
2020-07-21 12:21:42 +0300

    init commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Controller {
+
+    @FXML
+    private Button btn;
+
+    @FXML
+    private void click(ActionEvent event) {
+        btn.setText("You've clicked!");
+    }
+
+}
